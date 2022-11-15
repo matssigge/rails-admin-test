764a84375777240766bf2c7611656c361f687ed9
Mats Sigge <mats.sigge@gmail.com>
2022-11-15 14:52:06 +0100

    Create test widget in db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Widget.find_or_create_by(id: 1) do |w|
+  w.name = 'Test widget'
+end
